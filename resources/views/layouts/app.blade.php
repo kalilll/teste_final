013a67da95c470a8b91724ad16ac4749dafc13b6
kalillll <kalillopeslima@gmail.com>
2025-10-18 21:57:52 -0300

    create controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,75 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>@yield('title', 'Sistema de Controle Acadêmico')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-</head>
-<body>
-    <main>
-        <div class="container">
-    <div>
-        @auth
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"aria-haspopup="true" aria-expanded="false">Aluno</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="{{route('aluno.create')}}">Cadastrar</a>
-                    <a class="dropdown-item" href="{{route('aluno.index')}}">Listar</a>
-                </div>
-            </li>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"aria-haspopup="true" aria-expanded="false">Professor</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="{{route('professor.create')}}">Cadastrar</a>
-                    <a class="dropdown-item" href="{{route('professor.index')}}">Listar</a>
-                </div>
-            </li>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"aria-haspopup="true" aria-expanded="false">Curso</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="{{route('curso.create')}}">Cadastrar</a>
-                    <a class="dropdown-item" href="{{route('curso.index')}}">Listar</a>
-                </div>
-            </li>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"aria-haspopup="true" aria-expanded="false">Turma</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="{{route('turma.create')}}">Cadastrar</a>
-                    <a class="dropdown-item" href="{{route('turma.index')}}">Listar</a>
-                </div>
-            </li>
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
 
-            <li class="nav-item">
-                <a class="nav-link" href="https://media.tenor.com/xSXTkAlLjbgAAAAM/homer.gif">Link</a>
-            </li>
-
-            <li class='nav-item'>
-                <div>
-                    {{ Auth::user()->name }}
-                </div>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-danger">Sair</button>
-                </form>
-            </li>
-        </ul>
-        @endauth
-    </div>
-    @yield('content')
-<footer >
-        <p class="text-center">&copy; 2025 - Todos os direitos reservados</p>
-    </footer>
-    </div>
-    </main>
-
-    
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,7 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 require __DIR__.'/auth.php';
