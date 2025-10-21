34b63a42e2d14d914a0aa648015655038425694c
kalilll <kalillopeslima.com>
2025-10-20 23:10:13 -0300

    Footer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center border-b border-[#C2BEBE]">
+        <header>
+            <h1 class="text-4xl font-bold text-[#000] mb-4">
+                Publicações
+            </h1>
+        </header>
+    </div>
+    <div>
+        
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,79 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body>
+        <div class="flex-grow container-none mx-auto px-12 py-8">
+            <div class="grid grid-cols-4 w-full gap-10"> 
+                <!-- Perfil -->
+                <div class="p-6 h-screen">
+                    <div class="flex flex-col items-center">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+
+                        <img src="{{ Auth::guest() ? asset('imagens/logo/logo_sabor_do_brasil.png') : Auth::user()->foto}}" class="w-24 h-24 rounded-full mb-4 object-cover">
+
+
+                        <h2 class="text-xl font-bold text-[#000000]">
+                            {{ Auth::guest() ? 'Sabor do Brasil' : Auth::user()->nome }}
+                        </h2>
+
+                        <hr class="mb-4 border-3 border-[#D97014] w-3/4">
+
+                        <div class=" grid grid-cols-2 gap-5">
+                            <div class="text-center"><p>Quatidade <br> Likes</p></div>
+                            <div class="text-center"><p>Quatidade <br> Deslikes</p></div>
+                            
+                        </div>
+
                     </div>
-                </header>
-            @endisset
+                </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <!-- Principal -->
+                <main class="col-span-2 shadow-md p-6 border border-[#C2BEBE]">
+                    @yield('content')
+                </main>
+
+
+                <!-- Login -->
+                <div class="bg-white rounded-tr-2xl p-6">
+                    <div class="flex items-center mb-4 justify-center">
+                        <h2 class="text-lg font-bold text-[#1b1b18] dark:text-[#EDEDEC]">Fazer Login</h2>
+                    </div>
+                    <button class="w-full bg-[#f53003] hover:bg-[#d42a03] text-white py-2 rounded-lg transition duration-300">
+                        Entrar
+                    </button>
+                </div>
+
+            </div>
         </div>
-    </body>
+
+        <footer class="bg-[#d97014] text-[#FFFFFF] py-8">
+            <div class="container grid grid-cols-3 justify-center">
+
+                <div class="mb-4 text-left">
+                    <p class="text-2xl font-bold text-[#FFFFFF]">Sabor do Brasil</p>
+                </div>
+
+                <div class="flex justify-center pt-1.5 gap-20">
+                    <a href="">
+                        <img src="imagens\icones\Instagram.svg" alt="Facebook" class="h-7 w-7">
+                    </a>
+                    <a href="">
+                        <img src="imagens\icones\Twitter.svg" alt="Facebook" class="h-7 w-7">
+                    </a>
+                    <a href="">
+                        <img src="imagens\icones\Whatsapp.svg" alt="Facebook" class="h-7 w-7">
+                    </a>
+                    <a href="">
+                        <img src="imagens\icones\Globe.svg" alt="Facebook" class="h-7 w-7">
+                    </a>
+                    </div>
+
+                <div class="mb-4 text-right">
+                    <p class="text-2xl font-bold text-[#FFFFFF]">Copyright - 2024</p>
+                </div>
+
+            </div>
+        </footer>
+
+    </body> 
 </html>
